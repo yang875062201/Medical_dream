9b3a3879321dc983b47142ef44c8cdae8b4d8b98
DESKTOP-CR7G25F\杨大大 <87506220@qq.com>
2019-11-07 19:30:12 +0800

    修改后一点点小毛病

--- Medical_dream/app/src/main/java/com/example/medical_dream/ui/fragments/ShoppingFragment.java
+++ Medical_dream/app/src/main/java/com/example/medical_dream/ui/fragments/ShoppingFragment.java
@@ -18,29 +18,5 @@ import com.example.medical_dream.R;
 public class ShoppingFragment extends Fragment {
 
 
-    private View view;
-    private Toolbar mToolbar;
-    private TabLayout mTabLayout;
-    private FrameLayout mFrameLayout;
 
-    public ShoppingFragment() {
-        // Required empty public constructor
-    }
-
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        View view = inflater.inflate(R.layout.fragment_shopping, container, false);
-        initView(view);
-        return view;
-    }
-
-    private void initView(View view) {
-        mToolbar = (Toolbar) view.findViewById(R.id.toolbar);
-
-        mTabLayout = (TabLayout) view.findViewById(R.id.tabLayout);
-        mFrameLayout = (FrameLayout) view.findViewById(R.id.frameLayout);
-    }
 }
